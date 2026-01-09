8fadda11bf9f74524f2f9160ca4281c307b11713
Csaba Soros <csaba.sorosl@cushwake.com>
2026-01-09 15:50:21 +0100

    Github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/InvisibleDeck/', // <-- A REPO neve! Fontos!
+  base: "/InvisibleDeck/", // <-- A REPO neve! Fontos!
   plugins: [react()],
-})
+});
